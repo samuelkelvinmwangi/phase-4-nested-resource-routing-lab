eb769f1689c5223c30031cbd8e8c28dc7dee78fb
samuel <samuelkelvinmwangi@gmail.com>
2023-08-30 16:33:18 +0300

    making corrections

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,8 +1,33 @@
 class ItemsController < ApplicationController
+  before_action :set_user, only: [:index, :show, :create]
 
   def index
-    items = Item.all
-    render json: items, include: :user
+    items = @user.items
+    render json: items
   end
 
+  def show
+    item = @user.items.find(params[:id])
+    render json: item
+  end
+
+  def create
+    item = @user.items.new(item_params)
+
+    if item.save
+      render json: item, status: :created
+    else
+      render json: item.errors, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def set_user
+    @user = User.find(params[:user_id])
+  end
+
+  def item_params
+    params.require(:item).permit(:name, :description, :price)
+  end
 end
